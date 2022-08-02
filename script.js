049f9da284390b7a4571db75c0af33c0e49625a2
Ping Yu <pingyuuu@hotmail.com>
2022-08-02 19:37:16 -0400

    added animations to contact form

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+// PROJECT DEMO VIDEOS
+
 const demos = document.querySelectorAll('video')
 
-demos.forEach((e) => {
-    e.addEventListener('mouseover', function () {
+demos.forEach((demo) => {
+    demo.addEventListener('mouseover', function (e) {
         this.play();
     });
 
-    e.addEventListener('mouseleave', function () {
+    demo.addEventListener('mouseleave', function (e) {
         this.pause();
     });
+})
+
+// CONTACT FORM 
+
+const formInputs = document.querySelectorAll('.formInput')
+
+formInputs.forEach((formInput) => {
+    const label = document.querySelector(`[for="${formInput.id}"]`)
+
+    formInput.addEventListener('focus', () => {
+        formInput.classList.add('formInputFocused');
+        label.classList.add('inputLabelFocused');
+    })
+    formInput.addEventListener('blur', () => {
+        if (!formInput.value){
+            formInput.classList.remove('formInputFocused');
+            label.classList.remove('inputLabelFocused');
+        }
+    })
 })
